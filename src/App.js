3f00ba3d9a1894b5d0ea4a57dda9069da622f067
MuhammadShaheer1 <59112438+MuhammadShaheer1@users.noreply.github.com>
2021-06-29 18:40:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Student from './Student'
 function App() {
   return (
     <div className="app-header">
-      <Student name="Shaheer" university = "NUST"/>
+      <Student name="Muhammad Shaheer" university = "NUST"/>
     </div>
   );
 }
